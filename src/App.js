b43859489883d4c13c035ae28cbe4c3eef2eb08d
Jay0101 <tjr0101@gmail.com>
2020-12-11 02:49:16 -0700

    Added Library view to the application

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './styles/app.scss';
 
 //Adding Components
 import Player from './components/Player';
-import Song from './components/Song'
+import Song from './components/Song';
+import Library from './components/Library'
 
 //Import Data
 import data from './data'
@@ -12,11 +13,17 @@ import data from './data'
 function App() {
   const [songs,setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player currentSong={currentSong}/>
+      <Player 
+      currentSong={currentSong}
+      isPlaying = {isPlaying}
+      setIsPlaying = {setIsPlaying}      
+      />
+      <Library songs={songs}/>
     </div>
   );
 }
